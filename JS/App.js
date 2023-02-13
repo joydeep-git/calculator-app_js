d9428bef57ba037409a5f9eb8f5a4efd4552d1c0
joydeep-git <joydeep.accounts@tuta.io>
2023-02-13 16:47:26 +0530

    basic changes

---
+++ JS/App.js
@@ -0,0 +1,7 @@
+const buttons = document.getElementById("button");
+
+const result = document.getElementById('result');
+
+const clear = document.getElementById('clear');
+
+const backspace = document.getElementById('backspace');
