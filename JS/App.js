7f94668e1f293dce81b0d133fb00ba5bc713f9a7
joydeep-git <joydeep.accounts@tuta.io>
2023-02-14 00:53:33 +0530

    complete design changed and function added

--- JS/App.js
+++
@@ -1,7 +0,0 @@
-const buttons = document.getElementById("button");
-
-const result = document.getElementById('result');
-
-const clear = document.getElementById('clear');
-
-const backspace = document.getElementById('backspace');
